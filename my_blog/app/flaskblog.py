7478b80af3f66507cc8d6247929f8b472e925783
Ayushdhynai1947 <144094633+Ayushdhynai1947@users.noreply.github.com>
2024-06-16 13:26:20 +0530

    template work

--- my_blog/app/flaskblog.py
+++ my_blog/app/flaskblog.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template,url_for
+from my_blog import RegistrationForm ,LoginForm
 
 
 
+"""talkieTime"""
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = '0839e7253dfb7c08d6fc59d8ef127f22'
 
 posts =[
     {
@@ -11,7 +13,8 @@ posts =[
         'title': 'Blog Post 1',
         'content':'First post content',
         'date_poster':'April 20,2018'
-    },
+    }
+     ,
     {
         'auther':'Jane Doe',
         'title':'Blog post 2',
@@ -21,18 +24,29 @@ posts =[
 ]
 
 
-
-
-
 @app.route("/")
 @app.route("/home")
 def hello():
     return render_template('home.html',posts=posts)
 
-
 @app.route("/about")
 def about():
     return render_template('about.html',title='About')
+
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html',title='Regsiter', form=form)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html',title='Login', form=form)
+
+
+
 
 
 
