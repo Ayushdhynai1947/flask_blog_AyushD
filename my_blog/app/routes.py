35245a54bf587814b9832b0e49600e136c93390e
Ayushdhynai1947 <144094633+Ayushdhynai1947@users.noreply.github.com>
2024-07-08 22:22:08 +0530

    crud

--- my_blog/app/routes.py
+++ my_blog/app/routes.py
@@ -109,7 +109,8 @@ def new_post():
         flash('Your post has been created!','success')
         return redirect(url_for('home'))
     
-    return render_template('create_post.html', title='New Post' , form = form)
+    return render_template('create_post.html', title='New Post' ,
+                           form = form ,legend ='New Post')
 
 
 
@@ -119,12 +120,34 @@ def post(post_id):
     return render_template('post.html',title = post.title,post=post)
 
 
-@app.route("/post/<int:post_id>/update")
+@app.route("/post/<int:post_id>/update",methods=['GET', 'POST'])
 @login_required
 def update_post(post_id):
     post = Post.query.get_or_404(post_id)
     if post.author != current_user:
         abort(403)
     form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been update!','succeess')
+        return redirect(url_for('post' , post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
     return render_template('create_post.html', title='Update Post' , 
-                           form = form ,legend ='Update')
+                           form = form ,legend ='Update Post')
+
+
+
+@app.route("/post/<int:post_id>/delete",methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)   
+    db.session.commit()
+    flash('Your post has been deleted!','success')
+    return redirect(url_for('home'))
